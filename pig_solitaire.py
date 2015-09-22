aedbe088362c20fb7724d4d0214620302bab0694
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-22 18:04:49 -0400

    pig_solitaire

--- pig_solitaire.py
+++ pig_solitaire.py
@@ -50,6 +50,7 @@ class Player:
         self.dice = Dice()
         self.score = 0
         self.total_score = 0
+        self.count = 0
 
     def roll(self):
         return self.dice.roll()
@@ -63,12 +64,23 @@ class Player:
         return self.score
 
     def roll_or_hold(self):
-        response = input("Do you want to roll or hold? (Roll/Hold)".lower())
-        if response == 'hold':
-            self.total_score += self.score
-            self.score = 0
+        if self.count is not 0 and self.score == 0:
+            response = input("Do you want to roll or hold? (Roll/Hold)".lower())
+            if response == 'hold':
+                print("You hold")
+                self.total_score += self.score
+                self.score = 0
+                self.count = 0
+                return self.total_score
+            if response == 'roll':
+                print("You roll")
+                self.round_score()
+                print("your score this round: ", self.score)
+                print("your total score: ", self.total_score)
+                self.count += 1
+                return self.roll_or_hold()
+        else:
+            print(self.count)
+            print("You bust")
+            self.count = 0
             return self.total_score
-        if response == 'roll':
-            self.round_score()
-            print(self.score)
-            return self.roll_or_hold()
